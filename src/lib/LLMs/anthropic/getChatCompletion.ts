bd1013a21d439adf01e599a80455ff753aa493c1
Tomas178 <tomaspetronis77@gmail.com>
2026-04-13 10:22:53 +0300

    (Typing): Improve typing in LLMs/anthropic/getChatCompletion

--- src/lib/LLMs/anthropic/getChatCompletion.ts
+++ src/lib/LLMs/anthropic/getChatCompletion.ts
@@ -1,11 +1,13 @@
 import { INTERVIEW_SYSTEM_PROMPT } from '@/constants/LLMs/prompts';
 import { anthropic } from '@/lib/LLMs/anthropic/client';
 import type { ChatMessage, ChatSettings } from '@/types/chat';
+import type { APIPromise } from '@anthropic-ai/sdk';
+import type { Message } from '@anthropic-ai/sdk/resources';
 
 export function getChatCompletion(
   messages: ChatMessage[],
   settings: ChatSettings
-) {
+): APIPromise<Message> {
   return anthropic().messages.create({
     model: settings.model,
     system: INTERVIEW_SYSTEM_PROMPT,
