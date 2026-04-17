a7617b84a231f2126d25c37784489d17705d9b72
Tomas178 <tomaspetronis77@gmail.com>
2026-04-17 15:19:30 +0300

    (Redeploy): redeploy as variables and secrets had to be changed

--- src/constants/website.ts
+++ src/constants/website.ts
@@ -1,6 +1,6 @@
 export const WEBSITE_TITLE = 'BackendPrep';
 
 export const WEBSITE_DESCRIPTION =
-  'An interview practice app for software engineering positions in backend.';
+  'An interview practice app for software engineering positions in the backend.';
 
 export const WEBSITE_KEYWORDS = ['backend', 'interview', 'preperation'];
